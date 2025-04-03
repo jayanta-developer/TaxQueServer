4e0dea863545a049f2046730266c1cca2e780bff
jayanta dey <jayantadey398@gmail.com>
2025-04-03 11:32:21 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ const App = require("./App");
 const PORT = process.env.PORT;
 
 App.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT || 5000}`);
 });
